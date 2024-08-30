12c133952ff04c02168d1753ca5bced5f4b32a42
cthdarren <ctze.darren@gmail.com>
2024-08-30 13:32:11 +0200

    progress on time mode

--- clitype.go
+++ clitype.go
@@ -11,11 +11,32 @@ Welcome to CLI Type. To change the mode of typing, type :q
 
 Select a mode: `)
 }
+
+func timetype(){
+	var typed string
+	fmt.Println("testing words for you to type this is how is done if fan cup food lunch hungry rice snowboard goggles\r")	
+	fmt.Scan(&typed)
+	fmt.Println("type test complete")
+}
+
+func wordstype(){
+
+}
+
 func main(){
 	for {
-		var i int
+		var i string
 		printIntro()
 		fmt.Scan(&i)
-		fmt.Println(i)
+		if i == "1" {
+			fmt.Println("Chose Time")
+			timetype()
+		} else if i == "2" {
+			fmt.Println("Chose Words")
+		} else if i == ":q" {
+			fmt.Println("exiting...")
+		} else {
+			fmt.Println("Unknown command.")
+		}
 	}
 }
