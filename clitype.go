efa09cc6eeab340fa5a68a15a6eb5a81f70e81a8
cthdarren <ctze.darren@gmail.com>
2024-10-16 13:58:52 +0200

    basic typing test completed

--- clitype.go
+++ clitype.go
@@ -12,10 +12,14 @@ import (
 
 func printIntro() {
 	fmt.Print(`
+
+=========================================================================
 Welcome to CLI Type. To change the mode of typing, type :q
 
 [1] Time
 [2] Words
+
+=========================================================================
 
 Select a mode: `)
 }
@@ -29,9 +33,10 @@ func timetype() {
 	}()
 
 	fmt.Println("Press ESC to quit")
+
 	var text string = "The quick brown fox jumps over the lazy dog"
 	var maxLen int = len(text)
-	var count int = 0
+	var cursor_pos int = 0
 	var hist string = ""
 
 	fmt.Printf(text)
@@ -39,10 +44,7 @@ func timetype() {
 
 	for {
 		char, key, err := keyboard.GetKey()
-		count += 1
-		if count == maxLen{
-			break
-		}
+
 		if err != nil {
 			panic(err)
 		}
@@ -53,13 +55,28 @@ func timetype() {
 			hist += string(char)
 		}
 
-		fmt.Printf("\r" + hist + text[count:maxLen])
-		fmt.Printf("\033[%dD", maxLen - count)
-
 		if key == keyboard.KeyEsc {
-			fmt.Printf("Escaped")
 			break
 		}
+
+
+		if key == keyboard.KeyBackspace || key == keyboard.KeyBackspace2 {
+			if cursor_pos > 0{
+				cursor_pos -= 1
+			}
+			if len(hist) > 1 {
+				hist = hist[:len(hist)-2]
+			}
+		} else {
+			cursor_pos += 1
+		}
+
+		if cursor_pos == maxLen{
+			break
+		}
+
+		fmt.Printf("\r" + hist + text[cursor_pos:maxLen])
+		fmt.Printf("\033[%dD", maxLen - cursor_pos)
 
 	}
 }
